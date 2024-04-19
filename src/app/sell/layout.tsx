ebd5eef279fe9c6d57504cede61a8feedb5df4e2
SamWaho <wahomesamuel2003@gmail.com>
2024-04-19 09:13:04 +0300

    "Add Header and Footer components to Sell layout"
    This commit adds the `Header` and `Footer` components to the layout of the `sell` page. The layout now wraps the children content with these two components

--- src/app/sell/layout.tsx
+++ src/app/sell/layout.tsx
@@ -1,10 +1,18 @@
+import Footer from "@/components/footer/page";
+import Header from "@/components/shared/header/page";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 const layout = ({ children }: Props) => {
-  return <>{children}</>;
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default layout;
