0cd47ee2d9ce073ec0c1d2a7a1a50522c391fac5
SamWaho <wahomesamuel2003@gmail.com>
2024-04-19 08:37:31 +0300

    "Add background color to page container and update styling for package components"
    This commit updates the styling for the package components in the sell/packages page. It adds a background color to the page container and updates the styling for the

--- src/app/sell/packages/page.tsx
+++ src/app/sell/packages/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 bg-main-bg">
       <div className="grid grid-cols-2 gap-4">
         {packages.map((item) => (
           <div key={item.title} className="bg-white p-2 rounded-md shadow-md">
-            <h2>{item.title}</h2>
+            <h2 className="text-lg font-bold">{item.title}</h2>
             <div className="flex items-center justify-between mt-3">
               <h4 className="font-semibold">Days:</h4>
               <p>{item.days}</p>
@@ -21,7 +21,7 @@ const Page = () => {
               <h4 className="font-semibold">Price:</h4>
               <p>{item.price}</p>
             </div>
-            <Button className="mt-3">Subscribe</Button>
+            <Button className="mt-3 w-full h-8">Subscribe</Button>
           </div>
         ))}
       </div>
