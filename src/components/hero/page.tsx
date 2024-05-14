faaef9e6ba24cba42dc6fb3bf2a5ee4502d9d3c9
Mf.Sam <126422242+Samwaho@users.noreply.github.com>
2024-05-14 06:35:05 +0000

    "Add SliderComponent to Hero page to cycle through multiple banner images"

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -1,16 +1,54 @@
 import Image from "next/image";
 import React from "react";
+import SliderComponent from "../shared/slider";
 
 const Hero = () => {
   return (
     <div className="bg-white pt-2">
-      <Image
-        src="/Shopping cart-8.png"
-        alt="Banner Component"
-        width={1400}
-        height={1400}
-        className="w-full"
-      />
+      <SliderComponent slidesToShow={1} autoPlay={true}>
+        <Image
+          src="/Shopping cart-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+        <Image
+          src="/Agri-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+        <Image
+          src="/Beaty-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+        <Image
+          src="/car-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+        <Image
+          src="/Electronics-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+        <Image
+          src="/Real estate-8.png"
+          alt="Banner Component"
+          width={1400}
+          height={1400}
+          className="w-full"
+        />
+      </SliderComponent>
     </div>
   );
 };
