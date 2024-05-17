e16d8a4204a0b28da37b43699f93887e0880bd1c
Mf.Sam <126422242+Samwaho@users.noreply.github.com>
2024-05-17 11:38:22 +0000

    "Updated Loading component styles and layout"

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className=" min-h-screen p-4">
-      <Skeleton className="w-full h-[100px] lg:px-28" />
+    <div className=" min-h-screen p-4 lg:px-28">
+      <Skeleton className="w-full h-[100px] " />
       <Skeleton className="w-full lg:max-w-lg h-[40px] mx-4 mt-4" />
-      <div className="grid grid-cols-2 gap-3 mt-4">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 mt-4">
         <Skeleton className="w-full h-[200px]" />
         <Skeleton className="w-full h-[200px]" />
         <Skeleton className="w-full h-[200px]" />
