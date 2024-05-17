671718bb5ad9bb23f9f60ebdd3ee4d005232f7da
Mf.Sam <126422242+Samwaho@users.noreply.github.com>
2024-05-17 11:26:11 +0000

    "Update Categories page: modify h4 font size and add margin top to grid container on large screens"

--- src/components/categories/page.tsx
+++ src/components/categories/page.tsx
@@ -7,8 +7,8 @@ const Categories = async () => {
   const categories = await getCategories();
   return (
     <div className="mt-4 px-6 xl:px-28 font-semibold pb-8">
-      <h4 className="font-semibold">Categories</h4>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4">
+      <h4 className="font-semibold lg:text-xl">Categories</h4>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4 lg:mt-4">
         {categories?.map((category) => {
           return (
             <CategoryCard
