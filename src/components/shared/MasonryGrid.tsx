fa424d2d4da57fd42e81124e859b3738ea7a753d
Mf.Sam <126422242+Samwaho@users.noreply.github.com>
2024-05-20 04:33:35 +0000

    "Updated MasonryGrid component to use a breakpointColumnsObj instead of a fixed value for breakpointCols"

--- src/components/shared/MasonryGrid.tsx
+++ src/components/shared/MasonryGrid.tsx
@@ -6,9 +6,15 @@ type Props = {
   children: React.ReactNode;
 };
 const MasonryGrid = ({ children }: Props) => {
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 2,
+  };
   return (
     <Masonry
-      breakpointCols={4}
+      breakpointCols={breakpointColumnsObj}
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column"
     >
