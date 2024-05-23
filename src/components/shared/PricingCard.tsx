9f6a2a0f1e3cbc7010aa75fa0682ad88d83b28f7
Mf.Sam <126422242+Samwaho@users.noreply.github.com>
2024-05-23 16:53:10 +0000

    "Refactor PricingCard component: removed outer div, updated Card className, and changed Button className"

--- src/components/shared/PricingCard.tsx
+++ src/components/shared/PricingCard.tsx
@@ -16,32 +16,30 @@ type Props = {
 };
 const PricingCard = ({ title, price, days, ads }: Props) => {
   return (
-    <div className="p-4">
-      <Card className="w-[150px] mx-auto max-w-md bg-white text-gray-900 shadow-lg rounded-2xl dark:bg-gray-950 dark:text-gray-50">
-        <CardHeader className="bg-gradient-sky text-white rounded-t-2xl py-2 text-center">
-          <CardTitle className="text-xl font-bold">{title}</CardTitle>
-        </CardHeader>
-        <CardContent className="space-y-4 px-2 py-4">
-          <div className="flex items-baseline justify-center gap-2">
-            <span className="text-lg font-bold">Ksh.{price}</span>
-            <span className="text-gray-500">/m</span>
-          </div>
-          <div className="flex items-center justify-center gap-2">
-            <TagsIcon className="h-6 w-6 fill-sky-500" />
-            <span className="text-md font-bold">{ads} Ads</span>
-          </div>
-          <div className="flex items-center justify-center gap-2">
-            <CalendarDaysIcon className="h-6 w-6 fill-sky-500" />
-            <span className="text-md font-bold">{days}</span>
-          </div>
-        </CardContent>
-        <CardFooter className="bg-gray-100 px-2 py-2 rounded-b-2xl">
-          <Button className="bg-slate-800 rounded-xl p-2 mx-auto h-6">
-            Subscribe
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="w-[150px] mx-2 max-w-md bg-white text-gray-900 shadow-lg rounded-2xl dark:bg-gray-950 dark:text-gray-50">
+      <CardHeader className="bg-gradient-sky text-white rounded-t-2xl py-2 text-center">
+        <CardTitle className="text-xl font-bold">{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-4 px-2 py-4">
+        <div className="flex items-baseline justify-center gap-2">
+          <span className="text-lg font-bold">Ksh.{price}</span>
+          <span className="text-gray-500">/m</span>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <TagsIcon className="h-6 w-6 fill-sky-500" />
+          <span className="text-md font-bold">{ads} Ads</span>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <CalendarDaysIcon className="h-6 w-6 fill-sky-500" />
+          <span className="text-md font-bold">{days}</span>
+        </div>
+      </CardContent>
+      <CardFooter className="bg-gray-100 px-2 py-2 rounded-b-2xl">
+        <Button className="bg-slate-800 rounded-md px-4 mx-auto h-6">
+          Subscribe
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
